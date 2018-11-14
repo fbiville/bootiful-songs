5cbc876f103aa0e11cf5a9b927d8fa1fa6661f85
Sandrine Piriou <spiriou@pivotal.io>
2018-11-14 21:34:06 +0100

    Add basic video component

---
+++ interwebz/src/video/Video.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Video from './Video';
+
+it('displays a youtube video', () => {
+    const props = {
+        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    }
+
+    const video = document.createElement('div');
+    ReactDOM.render(<Video {...props} />, video);
+
+    const iframe = video.querySelector('iframe');
+    expect(iframe).toBeTruthy();
+    expect(iframe!.getAttribute('src')).toBe(props.url);
+});
---
+++ interwebz/src/video/Video.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+class Video extends React.Component<VideoProps> {
+    public render() {
+
+        const url = this.props.url;
+        return (
+            <div>
+                <iframe
+                width="560"
+                height="315"
+                src={url}
+                allow="autoplay; encrypted-media" />
+            </div>
+        );
+    }
+}
+
+class VideoProps {
+    url: string
+}
+
+export default Video;
